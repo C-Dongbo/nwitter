e8417ee05a84debc9b64bf0963d60a9e0208392f
C-Dongbo <chlehdrms3@gmail.com>
2021-08-27 11:05:43 +0900

    firebase connect

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyXfi35MQDOgNjjqNKsdlQT6xD__lTByU",
+  authDomain: "nwitter-ff568.firebaseapp.com",
+  projectId: "nwitter-ff568",
+  storageBucket: "nwitter-ff568.appspot.com",
+  messagingSenderId: "965039744632",
+  appId: "1:965039744632:web:1ba3e6ca733c736296ef8d"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from "./firebase" // firebase 9.0.0 아래 버전 사용해야 함 (npm install firebase@8.9.1)
+
+console.log(firebase)
 
 ReactDOM.render(
   <React.StrictMode>
