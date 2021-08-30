617d77e2dc5955d459fdf54a9862218dab6ffcad
C-Dongbo <chlehdrms3@gmail.com>
2021-08-30 11:36:24 +0900

    setting

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyXfi35MQDOgNjjqNKsdlQT6xD__lTByU",
-  authDomain: "nwitter-ff568.firebaseapp.com",
-  projectId: "nwitter-ff568",
-  storageBucket: "nwitter-ff568.appspot.com",
-  messagingSenderId: "965039744632",
-  appId: "1:965039744632:web:1ba3e6ca733c736296ef8d"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PRJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
---
+++ src/routes/Router.js
@@ -0,0 +1,13 @@
+import { HashRouter as Router, Route, Switch} from "react-router-dom"
+
+const AppRouter = () => {
+    return(
+        <Router>
+            <switch>
+                <Route />
+            </switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
