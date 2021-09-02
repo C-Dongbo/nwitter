bbee119f679d6823461c35cb064e06fd5ba104c7
C-Dongbo <chlehdrms3@gmail.com>
2021-09-02 14:31:27 +0900

    login service init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import firebase from "./firebase" // firebase 9.0.0 아래 버전 사용해야 함 (npm install firebase@8.9.1)
+import App from 'components/App';
+//import firebase from "./fbase" // firebase 9.0.0 아래 버전 사용해야 함 (npm install firebase@8.9.1)
 
-console.log(firebase)
 
 ReactDOM.render(
   <React.StrictMode>
