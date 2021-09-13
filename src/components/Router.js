638060b6ac21313a0bbd24e13eb4bb2564260925
C-Dongbo <chlehdrms3@gmail.com>
2021-09-13 12:20:22 +0900

    add social log-in and log-out function

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,24 @@
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import Auth from "routes/Auth"
 import Home from "routes/Home"
+import Profile from "./Profile"
+import Navigation from "./Navigation"
 
 const AppRouter = ({isLoggedIn}) => {
     
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <switch>
                 {isLoggedIn ?(
-                    <Route exact path = "/">
-                        <Home />
-                    </Route>
+                    <>
+                        <Route exact path = "/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                    </>
                 ) : (
                     <Route exact path = "/">
                         <Auth />
--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
-import "firebase/auth"
+import "firebase"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,5 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 //export default firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
 export const authService = firebase.auth();
